f86c6befcf5d4630ea44ac878fbfae37a45b72be
gavin fournier <gavinf2@learn.cssd.ab.ca>
2019-02-06 13:47:18 -0700

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Gavin Fournier on 2019-02-06.
+//  Copyright © 2019 Gavin Fournier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
