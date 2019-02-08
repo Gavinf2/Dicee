c9a7b66a4002bf452db8c4bfddb21f408e423a9b
gavin fournier <gavinf2@learn.cssd.ab.ca>
2019-02-08 12:27:53 -0700

    programmed the dice images to changed on push

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -13,22 +13,37 @@ class ViewController: UIViewController {
     var randomDiceIndex1 : Int = 0
     var randomDiceIndex2 : Int = 0
     
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
     @IBOutlet weak var diceimageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        updateDiceImages()
+        
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-    
-        randomDiceIndex1 = Int(arc4random_uniform(6))
-        randomDiceIndex2 = Int(arc4random_uniform(6))
         
-        print(randomDiceIndex1)
+        updateDiceImages()
         
     }
     
-}
+        
+    
+    func updateDiceImages(){
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+        diceimageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
+        
+        }
+        
+    }
+
+    
+
 
